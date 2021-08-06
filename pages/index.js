74824a5b6c7b4172c3625a1963092aed2da93f97
kelly <kelly@yahoo.com>
2021-08-07 01:57:17 +0800

    hometitle、水滴修改

--- pages/index.js
+++ pages/index.js
@@ -13,15 +13,15 @@ import styles from "../components/home/home.module.scss";
 import Hamburger from '../components/header/hamburger.js'
 
 export const fancontent = [
-  { title: "測試用文字1", text: "測試用內容" },
-  { title: "測試用文字2", text: "測試用內容" },
-  { title: "測試用文字3", text: "測試用內容" }, 
-  { title: "測試用文字4", text: "測試用內容" },
-  { title: "測試用文字5", text: "測試用內容" },
-  { title: "測試用文字6", text: "測試用內容" },
+  { id: "1",title: "測試用文字1", text: "測試用內容" },
+  { id: "2",title: "測試用文字2", text: "測試用內容" },
+  { id: "3",title: "測試用文字3", text: "測試用內容" }, 
+  { id: "4",title: "測試用文字4", text: "測試用內容" },
+  { id: "5",title: "測試用文字5", text: "測試用內容" },
+  { id: "6",title: "測試用文字6", text: "測試用內容" },
 ];
 
-const firstpage = ({data}) => {
+const firstpage = ({}) => {
   const { asPath, query } = useRouter();
     const fanitems = fancontent.map((e) => (
       <FanCard
@@ -79,27 +79,6 @@ const firstpage = ({data}) => {
   )
 }
 
-export async function getStaticProps() {
-  const res = await fetch(`${process.env.API_HOST}/api/v1/news`);
-  const json = await res.json();
-
-  const data = Array.isArray(json.data) ? json.data : json.data.news;
-
-  return {
-    props: {
-      data,
-    },
-    revalidate: 1,
-  };
-}
-
-export async function getStaticPaths() {
-
-  return {
-    paths: [{ params: { id: '1' } }],
-    fallback: false,
-  };
-}
 
 export default firstpage;
 
